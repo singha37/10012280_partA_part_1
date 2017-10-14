ccaba69007e48fc35236d52b36a71773498a9dc9
singha37 <singha37@g.toiohomai.ac.nz>
2017-10-14 15:28:28 +1300

    initial commit

--- script.js
+++ script.js
@@ -5,34 +5,37 @@ let pw = document.querySelector('#pw')
 let cpw = document.querySelector('#cpw')
 let submit = document.querySelector('#submit')
 
-submit.addEventListener("click", function () {
+alert("Welcome")
+  
+  submit.addEventListener("click", function () {
   if (check1() == true) {
     if (checkLength() == true) {
-      alert("Your Userrname and password is sent")
+      alert("Your Username and password is set")
+    }
+    else{
+      alert("check your username or password")
     }
   }
-})
-
-/*function to check userid & password*/
-function check1() {
-
-  if (pw.value == cpw.value) {
-    return true;
+});
+/*function to check password*/
+function check1(form) {
+if (pw.value == cpw.value) {
+    return(true)
   }
   else {
     alert("Error type your Password again")/*displays error message*/
   }
 
 }
-/*function to check userid & password*/
+/*function to check length*/
 function checkLength(form) {
 
-  if (un.value.length <= 8) {
-    alert('Your Username must be at least ' + minlength + ' characters long. Try again.');
+  if (un.value.length >= 8) {
+    return(true)
   }
   else {
-    return false;
+    alert("Your Username must be at least 8 characters long. Try again.")
+    checkLength.addEventListener('click', (e) => {checkLengthFunction(); e.preventDefault()
+     })
   }
-
 }
-//Event Listener
